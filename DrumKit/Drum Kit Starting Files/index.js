ae5c9138ac614b5273344d7fe1896bf586108e44
NoJaniNoParty <182005668+NoJaniNoParty@users.noreply.github.com>
2024-10-06 18:39:15 +0100

    DrumKit

--- DrumKit/Drum Kit Starting Files/index.js
+++ DrumKit/Drum Kit Starting Files/index.js
@@ -1,10 +1,62 @@
 var numofbuttons = document.querySelectorAll(".drum").length;
-
 for(var i =0; i< numofbuttons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        audio.play();
+
+        var buttonpressed = this.innerHTML;
+     
+        makesound(buttonpressed);
+        anim(buttonpressed);
     })
+
+ 
+}
+document.addEventListener("keydown", function (event){
+    makesound(event.key);
+    anim(event.key);
+})
+
+
+function makesound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
 }
 
 
-var audio = new Audio('./sounds/tom-1.mp3');
+function anim(butpressed){
+    var active = document.querySelector("."+ butpressed);
+    active.classList.add("pressed");
+
+    setTimeout(function(){
+        active.classList.remove("pressed");
+    },100);
+}
