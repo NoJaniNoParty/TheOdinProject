a48a1224f53dfbe0c7d299962e7047a448401d72
NoJaniNoParty <182005668+NoJaniNoParty@users.noreply.github.com>
2024-10-08 15:57:59 +0100

    Javascrip tasks

---
+++ JavaScript tasks/fizzbuzz.js
@@ -0,0 +1,21 @@
+//TASK: Write a program that allows the user to enter a number, print each number between one and the number the user entered, but for numbers that divide by 3 without a remainder print Fizz instead. For numbers that divide by 5 without a remainder print Buzz and finally for numbers that divide by both 3 and 5 without a remainder print FizzBuzz.
+/*
+let input = prompt("Write your number");
+
+
+for(i=1;i<=input;i++){
+    if(i % 3 ===0 & i % 5 === 0){
+        console.log("fizzbuzz");
+    }
+    else if(i % 3 === 0){
+        console.log("fizz");
+    }
+    else if(i % 5 === 0){
+        console.log("buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
+
+*/
