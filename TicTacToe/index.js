fd5b06864d806578c85fb1cb9cca6bbc051202b2
NoJaniNoParty <182005668+NoJaniNoParty@users.noreply.github.com>
2024-11-10 18:27:14 +0000

    Everything JS works now

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,162 +1,220 @@
-
-
-
-
-const Gameboard = (function(){
-    let turnTracker = "p1";
-    let noWinner = true;
-    let box = document.querySelectorAll(".box");
-    let popup = document.querySelector("#popup");
-    const popupMessage = document.querySelector("#popup p:nth-child(2)");
-    popup.style.visibility = "hidden";
-    
-    const gameboardAr = [1,2,3,4,5,6,7,8,9];
-    const conditions = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9],
-        [3,5,7],
-        [1,5,9]
-    ];
-
-  
-    function addingToBoard(e){
-           
-            if(turnTracker === "p1"){
-                Players.player1.push(Number(e.getAttribute('value')));
-                console.log(Players.player1);
-                checkForWinner(turnTracker);
-                e.innerHTML = "x";
-                turnTracker = "p2";
-                
-                
-                i++;
-                e.classList.add("disabled");
-                
-            }
-            else{
-                Players.player2.push(Number(e.getAttribute('value')));
-                console.log(Players.player2);
-                checkForWinner(turnTracker);
-                e.innerHTML = "o";
-                
-                turnTracker = "p1";
-               
-              
-                i++;
-                e.classList.add("disabled");
-                
-            }
-    }
-    let tempP1 =[];
-    let tempP2 = [];
-    function checkForWinner(turn){
-       
-       if(turn === "p1"){
-        Players.player1.sort();
+    const Gameboard = (function(){
+        let turnTracker = "p1";
+        let resetButton = document.querySelector("#playAgain");
+        let gameOver = false;
+        let box = document.querySelectorAll(".box");
+        let popup = document.querySelector("#popup");
+        const popupMessage = document.querySelector("#popup p:nth-child(2)");
+        popup.style.visibility = "hidden";
         
-        for(let i = 0; i < conditions.length; i++){
+        const gameboardAr = [1,2,3,4,5,6,7,8,9];
+        const conditions = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [3,5,7],
+            [1,5,9]
+        ];
+
+        const GameStatus = {noWinner : true};
+        function addingToBoard(e){
             
-            for(let j = 0; j < Players.player1.length;j++){
-               
-                tempP1 = Players.player1.slice(j,j+3);
-                if(tempP1.length >= 3){
-                    if(tempP1.every(value => conditions[i].includes(value))){
-                        console.log("prvi dobio");
+                if(turnTracker === "p1"){
+                    Players.player1.push(Number(e.getAttribute('value')));
+                    console.log(Players.player1);
+                    if(checkForWinner(Players.player1)){
+                        Players.winTracker = 0;
                         endGame("Player 1 has won!!!");
-                        noWinner = false;
+                       
+                        GameStatus.noWinner = false;
+                        gameOver = true;
                     }
-                }
-               
-            }
-        }
-       }
-       else{
-        Players.player2.sort();
-        
-        for(let i = 0; i < conditions.length; i++){
-            
-            for(let j = 0; j < Players.player2.length;j++){
-               
-                tempP2 = Players.player2.slice(j,j+3);
-                if(tempP2.length >= 3){
-                    if(tempP2.every(value => conditions[i].includes(value))){
-                        console.log("drugi dobio");
-                        endGame("Player 2 has won!!!");
-                        noWinner = false;
+                    else{
+                        turnTracker = "p2";
                     }
-                }
-               
-            }
-        }
-       }
-
-    }
-
-    function endGame(winner){
-        box.forEach(e =>{
-            e.classList.add("disabled");
-            popup.style.visibility = "visible";
-            popupMessage.innerHTML = winner;
-            
-        })
-    }
-
-  
-    let i = 0;
-       
-        box.forEach(e => {
-            e.addEventListener("click", function(){
-                if(i === 8){
-                    if(noWinner){
-                        popupMessage.innerHTML = "It's a draw";
-                    }
-                    box.forEach(e =>{
-                        e.classList.add("disabled");
-                        popup.style.visibility = "visible";
-                        
-                        
-                    })
-                    popup.style.visibility = "visible";
-                        addingToBoard(e);
-                }
-                else{
-                    addingToBoard(e);
+                    
+                    e.innerHTML = "x";
+                    i++;
+                    e.classList.add("disabled");
+                    
                     
                 }
-               
+                else{
+                    Players.player2.push(Number(e.getAttribute('value')));
+                    console.log(Players.player2);
+                    if(checkForWinner(Players.player2)){
+                        endGame("Player 2 has won!!!");
+                        Players.winTracker = 1;
+                        GameStatus.noWinner = false;
+                        gameOver = true;
+                       
+                    }
+                    else{
+                        turnTracker = "p1";
+                    }
+                    
+                    e.innerHTML = "o";              
+                    i++;
+                    e.classList.add("disabled");
+
+                    
+                    
+                }
+                if(gameOver){
+                    Players.printResult(gameOver);
+                }
+            
+        }
+
+    //CHECKFORWINNER WARNING - the commented part worked mostly, but fell short when the game went on to 4 array items on some occasion it would not end the game
+    // let tempP1 =[];
+        //let tempP2 = [];
+        function checkForWinner(playerMoves){
+            playerMoves.sort();
+
+            return conditions.some(condition => condition.every(value => playerMoves.includes(value)));
+
+
+
+        /*
+        if(turn === "p1"){
+            Players.player1.sort();
+            
+            for(let i = 0; i < conditions.length; i++){
+                
+                for(let j = 0; j < Players.player1.length;j++){
+                
+                    tempP1 = Players.player1.slice(j,j+3);
+                    if(Players.player1.length >= 3){
+                        if(Players.player1.every(value => conditions[i].includes(value))){
+                            console.log("prvi dobio");
+                            endGame("Player 1 has won!!!");
+                            noWinner = false;
+                        }
+                    }
+                
+                }
+            }
+        }
+            
+        else{
+            Players.player2.sort();
+            
+            for(let i = 0; i < conditions.length; i++){
+                
+                for(let j = 0; j < Players.player2.length;j++){
+                
+                    tempP2 = Players.player2.slice(j,j+3);
+                    if(tempP2.length >= 3){
+                        if(tempP2.every(value => conditions[i].includes(value))){
+                            console.log("drugi dobio");
+                            endGame("Player 2 has won!!!");
+                            noWinner = false;
+                        }
+                    }
+                
+                }
+            }
+        }
+            */
+        }
+
+        function endGame(winner){
+            box.forEach(e =>{
+                e.classList.add("disabled");
+                popup.style.visibility = "visible";
+                popupMessage.innerHTML = winner;
+                
             })
-        });
-       
-      
-       
-
-  
+        }
 
     
+        let i = 0;
+    // no winner at the end finishing the game
+            box.forEach(e => {
+                e.addEventListener("click", function(){
+                
+                    if(i === 8){
+                        if(GameStatus.noWinner){
+                            popupMessage.innerHTML = "It's a draw";
+                            
+                        }
+                        gameOver = true;
+                        popup.style.visibility = "visible";
+                            addingToBoard(e);
+                    }
+                    else{
+                        addingToBoard(e);
+                        
+                    }
+                
+                })
+            });
+    // reseting the game to the start
+            resetButton.addEventListener("click", function(){
+                i = 0;
+                popup.style.visibility = "hidden";
+                Players.player1.length = 0;
+                Players.player2.length = 0;
+                turnTracker = "p1";
+                GameStatus.noWinner = true;
+                gameOver = false;
+                Players.winTracker = null;
+                box.forEach(e => {
+                    e.classList.remove("disabled");
+                    e.innerHTML = "";
+                });
+            });
+        
+        return {gameboardAr, GameStatus};
 
 
-    return {gameboardAr};
+    })();
+
+    const Players = (function(){
+
+        const player1 = [];
+        const player2 = [];
 
 
-})();
-
-const Players = (function(){
-
-    const player1 = [];
-    const player2 = [];
+        let p1Wins = 0;
+        let p2Wins = 0;
+        let winTracker = null;
+        let round = 1;
 
 
-    
-    
+        function printResult(detectGameover){
+            console.log(detectGameover);
+            if(Players.winTracker === 0){
+                if(round % 2 === 1){
+                    p1Wins++;
+                }
+                else{
+                    p2Wins++;
+                }
+                
+            }
+            if(Players.winTracker === 1){
+                if(round % 2 === 1){
+                    p2Wins++;
+                }
+                else{
+                    p1Wins++;
+                }
+            }
+            round++;
+            winTracker = null;
+            console.log("Player 1 wins:" + p1Wins);
+            console.log("Player 2 wins:" + p2Wins);
+
+        }
+
+        return {player1, player2, winTracker,  printResult};
 
 
-    return {player1, player2};
-
-
-})();
+    })();
 
 
